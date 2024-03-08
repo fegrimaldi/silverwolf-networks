530b4000d0017945636ee142c80775b3d483199a
Fabricio E. Grimaldi <fgrimaldi@gluware.com>
2024-03-08 12:29:56 -0500

    fixed bug

--- actions/get_bgp_state.py
+++ actions/get_bgp_state.py
@@ -5,11 +5,8 @@ from lib.device import Device
 class GetBgpState(action.BaseAction):
     def run(self, **parameters):
         self.conn_info["host"] = parameters["host"]
-
         self.device = Device(conn_info=self.conn_info)
-
         output = str(self.device.get_bgp_state())
-
         self.device.session.disconnect()
 
         return output
