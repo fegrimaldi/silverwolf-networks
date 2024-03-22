a2a21fdeac7cf5f2517dc4cb6ac5b9932e68ec87
Fabricio E. Grimaldi <34732205+fegrimaldi@users.noreply.github.com>
2024-03-21 19:53:40 -0700

    added web methods

---
+++ actions/get_glu_device_id.py
@@ -0,0 +1,18 @@
+from lib import action
+from lib.webmethod import WebMethod
+
+
+class GetGluDeviceId(action.BaseAction):
+    def run(self, **parameters):
+        self.web_method = WebMethod()
+        response_raw = self.web_method.call(
+            "GET",
+            f"{self.glu_base_url}/api/devices",
+            {
+                "orgId": "b131860f-ed86-4a87-a896-3a140e2c4ea9",
+                "name": parameters["device_name"],
+            },
+            auth=self.auth,
+        )
+        response = response_raw.json()
+        return response[0]["id"]
--- actions/lib/action.py
+++ actions/lib/action.py
@@ -6,6 +6,8 @@ class BaseAction(Action):
         super(BaseAction, self).__init__(config)
         self._username = self.config["username"]
         self._password = self.config["password"]
+        self.auth = (self.config["glu_username"], self.config["glu_password"])
+        self.glu_base_url = self.config["glu_base_url"]
 
         self.conn_info = {
             "device_type": "cisco_nxos",
