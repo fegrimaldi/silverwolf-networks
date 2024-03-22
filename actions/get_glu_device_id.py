34ff2dffd4a18f0a65e3d0a9493fdc610df8a9ae
Fabricio E. Grimaldi <34732205+fegrimaldi@users.noreply.github.com>
2024-03-21 22:07:06 -0700

    Update web method

--- actions/get_glu_device_id.py
+++ actions/get_glu_device_id.py
@@ -6,13 +6,14 @@ class GetGluDeviceId(action.BaseAction):
     def run(self, **parameters):
         self.web_method = WebMethod(verify=False)
         response_raw = self.web_method.call(
-            "GET",
-            f"{self.glu_base_url}/api/devices",
-            {
+            method="GET",
+            url=f"{self.glu_base_url}/api/devices",
+            params={
                 "orgId": "b131860f-ed86-4a87-a896-3a140e2c4ea9",
                 "name": parameters["device_name"],
             },
             headers=None,
+            payload=None,
             auth=self.glu_auth,
         )
         if response_raw is not None:
